b2d567fba4d772b6894ed2bfe4eba1feab55adee
rfelipesp <rfelipesp@gmail.com>
2024-04-15 22:09:49 -0300

    Update class

---
+++ src/main/java/com/caseanalitica/commons/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.caseanalitica.commons;
+
+public class ErrorResponse {
+
+    private String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
